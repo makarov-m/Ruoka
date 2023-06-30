77d42cce6927e5e8f32bbaea83ab58a7f7e723e7
Maxim Makarov <pro100maxim@gmail.com>
2023-06-30 20:34:07 +0300

    update 30 June

--- LottaBot/layer_2/bot.py
+++ LottaBot/layer_2/bot.py
@@ -1,33 +1,39 @@
-#!/usr/bin/env python
-import os
+"""
+Simple echo Telegram Bot example on Aiogram framework using AWS API
+Gateway & Lambda.
+"""
+
+
+import asyncio
 import logging
+import os
+
 import pandas as pd
 import csv
 import boto3
-import asyncio
-from datetime import datetime
-from io import StringIO
-from dotenv import load_dotenv
-from cachetools import TTLCache
-from aiogram import Bot, Dispatcher, executor, types
+
+from aiogram import Bot, Dispatcher, types
+
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.handler import CancelHandler
 from aiogram.dispatcher.middlewares import BaseMiddleware
 
-log = logging.getLogger(__name__)
-log.setLevel(os.environ.get('LOGGING_LEVEL', 'INFO').upper())
-cache = TTLCache(maxsize=float('inf'), ttl=0.5)
 
-# Initialize bot and dispatcher
-load_dotenv()
-token = os.getenv('TELEGRAM_LOTTA_TOKEN')
-if not token:
-    exit("Error: no token provided")
-bot = Bot(token=token)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+from datetime import datetime
+from io import StringIO
+from dotenv import load_dotenv
+from cachetools import TTLCache
+
+from dynamodb_connect import DynamoDBStorage
+
+
+# Logger initialization and logging level setting
+log = logging.getLogger(__name__)
+log.setLevel(os.environ.get('LOGGING_LEVEL', 'DEBUG').upper())
+# cache = TTLCache(maxsize=float('inf'), ttl=0.5)
 
 # Keyboard buttons
 lang_fi = KeyboardButton('FI')
@@ -46,113 +52,136 @@ class UserState(StatesGroup):
     restaurant = State()
     stopped = State()
 
-# Middleware for throttling. Ignores any repeated requests for 0.5 sec
-class ThrottleMiddleware(BaseMiddleware):
-    async def on_process_message(self, message: types.Message, data: dict):
-        if not cache.get(message.chat.id):  # It is assumed that the bot does NOT work in groups
-            cache[message.chat.id] = True   # There is no entry in the cache, create
-            return
-        else:  # skip processing
-            raise CancelHandler
-        
-dp.middleware.setup(ThrottleMiddleware())       
-
 # Read menu function
-def read_menu(restaurant: str, lang: str, date: str):
-    s3 = boto3.client('s3')
-    bucket_name = 'ruokabot'
-    object_key = f'{restaurant}.csv'
-    response = s3.get_object(Bucket=bucket_name, Key=object_key)
-    body = response['Body']
-    csv_string = body.read().decode('utf-8')
-    df = pd.read_csv(StringIO(csv_string))
-    df['Date'] = df['Date'].astype(str)
-    df['Date'] = pd.to_datetime(df['Date'], format='%d.%m')
-    df['Date'] = df['Date'].dt.strftime('%d.%m')
-    query_weekday = df[(df["Lang"]==lang)&(df["Date"]==date)]["Weekday"].drop_duplicates()
-    query_date = df[(df["Lang"]==lang)&(df["Date"]==date)]["Date"].drop_duplicates()
-    query_time = df[(df["Lang"] == lang) & (df["Date"] == date)]["LunchTime"].drop_duplicates()
-    query_menu = df[(df["Lang"] == lang) & (df["Date"] == date)]["Menu"].drop_duplicates()
-    query_price = df[(df["Lang"] == lang) & (df["Date"] == date)]["Price"].drop_duplicates()
-    query_link = df[(df["Lang"] == lang) & (df["Date"] == date)]["MenuLink"].drop_duplicates()
-    pd.set_option('display.max_colwidth', None)
-    return (
-        query_weekday.to_string(index=False), 
-        query_date.to_string(index=False), 
-        query_time.to_string(index=False), 
-        query_menu.to_string(index=False), 
-        query_price.to_string(index=False), 
-        query_link.to_string(index=False)
-        )
+# def read_menu(restaurant: str, lang: str, date: str):
+#     s3 = boto3.client('s3')
+#     bucket_name = 'ruokabot'
+#     object_key = f'{restaurant}.csv'
+#     response = s3.get_object(Bucket=bucket_name, Key=object_key)
+#     body = response['Body']
+#     csv_string = body.read().decode('utf-8')
+#     df = pd.read_csv(StringIO(csv_string))
+#     df['Date'] = df['Date'].astype(str)
+#     df['Date'] = pd.to_datetime(df['Date'], format='%d.%m')
+#     df['Date'] = df['Date'].dt.strftime('%d.%m')
+#     query_weekday = df[(df["Lang"]==lang)&(df["Date"]==date)]["Weekday"].drop_duplicates()
+#     query_date = df[(df["Lang"]==lang)&(df["Date"]==date)]["Date"].drop_duplicates()
+#     query_time = df[(df["Lang"] == lang) & (df["Date"] == date)]["LunchTime"].drop_duplicates()
+#     query_menu = df[(df["Lang"] == lang) & (df["Date"] == date)]["Menu"].drop_duplicates()
+#     query_price = df[(df["Lang"] == lang) & (df["Date"] == date)]["Price"].drop_duplicates()
+#     query_link = df[(df["Lang"] == lang) & (df["Date"] == date)]["MenuLink"].drop_duplicates()
+#     pd.set_option('display.max_colwidth', None)
+#     return (
+#         query_weekday.to_string(index=False),
+#         query_date.to_string(index=False),
+#         query_time.to_string(index=False),
+#         query_menu.to_string(index=False),
+#         query_price.to_string(index=False),
+#         query_link.to_string(index=False)
+#     )
 
 # Handlers
-@dp.message_handler(commands=['start'], state=UserState.stopped)
-async def cmd_start_after_stop(message: types.Message, state: State):
+async def cmd_start(message: types.Message, state: State):
     await message.answer("Bot started. Select Language", reply_markup=keyboard_lang)
     # Set the user state to 'language'
     await UserState.language.set()
-
-@dp.message_handler(commands=['stop'], state="*")
+    
 async def cmd_stop(message: types.Message, state: State):
     await message.answer("Bot stopped.")
     # Clear all states and set 'stopped' state
     await state.finish()
     await UserState.stopped.set()
-
-@dp.message_handler(state=UserState.language)
+    log.debug("Stop command executed.")
+    
 async def process_language(message: types.Message, state: State):
     if message.text in ["FI", "EN", "RU"]:
         await state.update_data(language=message.text)
         await message.answer("Select Restaurant", reply_markup=keyboard_rest)
         # Transition the state from 'language' to 'restaurant'
-        await UserState.restaurant.set()
+        await UserState.next()
     else:
         await message.answer("Invalid language selection")
+    log.debug("process_language function executed.")
 
-@dp.message_handler(state=UserState.restaurant)
-async def process_restaurant(message: types.Message, state: State):
-    if message.text in ["Wolkoff", "Kitchen", "Kehruuhuone"]:
-        # Access the user's selected language from state data
-        user_data = await state.get_data()
-        language = user_data.get('language')
-        # Set the date parameter to the current date in the format '%d.%m'
-        date = datetime.now().strftime('%d.%m')
-        # Call the read_menu function with appropriate arguments
-        menu = read_menu(message.text, language, date)
-        await message.answer(menu)
-    else:
-        await message.answer("Invalid restaurant selection")
+        
+# async def process_restaurant(message: types.Message, state: State):
+#     if message.text in ["Wolkoff", "Kitchen", "Kehruuhuone"]:
+#         # Access the user's selected language from state data
+#         user_data = await state.get_data()
+#         language = user_data.get('language')
+#         # Set the date parameter to the current date in the format '%d.%m'
+#         date = datetime.now().strftime('%d.%m')
+#         # Call the read_menu function with appropriate arguments
+#         menu = read_menu(message.text, language, date)
+#         # Format the menu information into a string
+#         menu_text = f"Weekday: {menu[0]}\nDate: {menu[1]}\nTime: {menu[2]}\nMenu: {menu[3]}\nPrice: {menu[4]}\nLink: {menu[5]}"
+#         await message.answer(menu_text)
+#         # Clear all states and set 'stopped' state
+#         await state.finish()
+#         await UserState.stopped.set()
+#     else:
+#         await message.answer("Invalid restaurant selection")
+#     log.debug("Process_restaurant function executed.")
 
-# Main function
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
 
-# async def process_event(event, dp: Dispatcher):
-#     """
-#     Converting an AWS Lambda event to an update and handling that
-#     update.
-#     """
-#     log.debug('Update: ' + str(event))
-#     Bot.set_current(dp.bot)
-#     update = types.Update.to_object(event)
-#     await dp.process_update(update)
+# AWS Lambda funcs
+async def register_handlers(dp: Dispatcher):
+    """Registration of all handlers before processing update."""
+    dp.register_message_handler(cmd_start, commands=['start'])
+    dp.register_message_handler(process_language, state=UserState.language)  # Add state argument to register the handler in the 'language' state
+    # dp.register_message_handler(process_restaurant, state=UserState.restaurant)
+    
+    # Log the registered handlers
+    # handlers = dp.message_handlers.handlers
+    # for handler in handlers:
+    #     log.debug(f"Registered handler: {handler.handler.__name__ if hasattr(handler, 'handler') and hasattr(handler.handler, '__name__') else None}, Commands: {handler.filters.commands if hasattr(handler, 'filters') and hasattr(handler.filters, 'commands') else None}, State: {handler.filters.state if hasattr(handler, 'filters') and hasattr(handler.filters, 'state') else None}")
+    log.debug('Handlers are registered.')
 
-# async def main(event):
-#     """
-#     Asynchronous wrapper for initializing the bot and dispatcher,
-#     and launching subsequent functions.
-#     """
 
-#     # Bot and dispatcher initialization
-#     # bot = Bot(os.environ.get('TELEGRAM_LOTTA_TOKEN'))
-#     # storage = MemoryStorage()
-#     # dp = Dispatcher(bot, storage=storage)
-#     await process_event(event, dp)
 
-#     return 'ok'
 
-# def lambda_handler(event):
-#     """AWS Lambda handler."""
+async def process_event(event, dp: Dispatcher):
+    """
+    Converting an AWS Lambda event to an update and handling that update.
+    """
+    log.debug('Update: ' + str(event))
 
-#     return asyncio.get_event_loop().run_until_complete(main(event))
+    # Create a dummy bot object to associate with the dispatcher
+    bot = Bot(token=os.environ.get('TOKEN'))
+    dp.bot = bot
 
+    # Create an instance of FSMContext
+    state = dp.current_state(chat=event['message']['chat']['id'], user=event['message']['from']['id'])
+
+    # Convert the event to an update
+    update = types.Update.to_object(event)
+
+    # Set the current bot instance
+    Bot.set_current(bot)
+
+    # Process the update with the dispatcher
+    await dp.process_update(update)
+
+    log.debug('Event processing complete.')
+
+
+
+async def main(event):
+    """
+    Asynchronous wrapper for initializing the bot and dispatcher,
+    and launching subsequent functions.
+    """
+    # Bot and dispatcher initialization
+    bot = Bot(token=os.environ.get('TOKEN'))
+    storage = MemoryStorage()
+    dp = Dispatcher(bot, storage=storage)
+    Dispatcher.set_current(dp)
+    await register_handlers(dp)
+    await process_event(event, dp)
+
+    return 'ok'
+
+    
+def lambda_handler(event, context):
+    """AWS Lambda handler."""
+    return asyncio.get_event_loop().run_until_complete(main(event))
--- README.md
+++ README.md
@@ -2,21 +2,31 @@
 
 ## App architecture
 
+![](pics/lottabot.png)
 
 ## create aws layer for scraping
 
 0. Create Lambda Function in AWS
 
-1. run the following commands:
-```bash
-cd layer_1                                   # move to the folder
-mkdir python                                 # create folder
-pip uninstall numpy                          # uninstall your local numpy
-pip install -r requirements.txt -t ./python  # create a package from requirements to new python folder
-```
-2. make sure that you don't have numpy 1.2.5. in your lambda layer, if exists then delete folders related to numpy. It can cause an error with Python v 3.9.
+![](pics/CreateLambda.png)
 
-3. run the following commands:
+more info: https://docs.aws.amazon.com/lambda/latest/dg/getting-started.html
+
+1. Copy the repository to your local machine:
+```bash
+git clone https://github.com/{{ username }}/.git
+```
+
+2. run the following commands:
+```bash
+cd layer_1                                  
+mkdir python                                 
+pip uninstall numpy                          
+pip install -r requirements.txt -t ./python 
+```
+3. make sure that you don't have numpy 1.2.5. in your lambda layer, if exists then delete folders related to numpy. It can cause an error with Python v 3.9.
+
+4. run the following commands:
 ```bash
 # move files from layer_1 to python dir
 mv scrapper_kehru_toCSV.py python/
@@ -27,24 +37,34 @@ mv .env python
 zip -r ../layer_1.zip .
 ```
 
-4. Attach the layers:
-- layer that you have created using ZIP file
-- layer that exists in AWS (AWSSDKPandas-Python39-Arm64) where Python and Numpy compatibility is configured
+4. Create layer in AWS and upload using ZIP file:
+![](pics/CreateLayers.png)
 
-some useful links about layers in AWS
-#### about layers
-https://docs.aws.amazon.com/lambda/latest/dg/configuration-layers.html#configuration-layers-path
-https://www.youtube.com/watch?v=uiRATBv8IQA&t=280s
-https://medium.com/the-cloud-architect/getting-started-with-aws-lambda-layers-for-python-6e10b1f9a5d
-#### about python package
-https://docs.aws.amazon.com/lambda/latest/dg/python-package.html
+#### here some useful links about python packages in AWS:
+- https://docs.aws.amazon.com/lambda/latest/dg/python-package.html
+
+5. Attach layers to your Lambda function:
+- layer that you have added
+- layer that exists in AWS (AWSSDKPandas-Python39-Arm64) where Python and Numpy compatibility is configured
+![](pics/AttachLayers.png)
+
+#### here some useful links about layers in AWS
+- https://docs.aws.amazon.com/lambda/latest/dg/configuration-layers.html#configuration-layers-path
+
+- https://www.youtube.com/watch?v=uiRATBv8IQA&t=280s
+
+- https://medium.com/the-cloud-architect/getting-started-with-aws-lambda-layers-for-python-6e10b1f9a5d
 
 5. Give all nececcary permisions to Lambda for reading and writing in AWS S3 service
+
 #### additing policies in AWS
 https://towardsdatascience.com/how-to-upload-and-download-files-from-aws-s3-using-python-2022-4c9b787b15f2
 #### adding policies to AWS lambda
 https://repost.aws/knowledge-center/lambda-execution-role-s3-bucket
 
+![](pics/Permission1.png)
+![](pics/Permission2.png)
+![](pics/Permission3.png)
 
 # Layer 2 
 cd layer_2
